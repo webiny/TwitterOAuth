a9fe614f5288cf08532d6ab2becccfc727e22742
SvenAlHamad <sven@webiny.com>
2015-02-05 03:37:51 +0000

    Version bumped to 1.2.1

--- Tests/TwitterOAuthLoaderTest.php
+++ Tests/TwitterOAuthLoaderTest.php
@@ -7,31 +7,23 @@
 
 namespace Webiny\Component\TwitterOAuth\Tests;
 
+use Webiny\Component\Http\Request;
 use Webiny\Component\TwitterOAuth\TwitterOAuth;
 use Webiny\Component\TwitterOAuth\TwitterOAuthLoader;
+use Webiny\Component\TwitterOAuth\Bridge\TwitterOAuth as Bridge;
 
 
 class TwitterOAuthLoaderTest extends \PHPUnit_Framework_TestCase
 {
 
-    const CONFIG = '/ExampleConfig.yaml';
-
     public function testGetInstance()
     {
-        TwitterOAuth::setConfig(realpath(__DIR__ . '/' . self::CONFIG));
+        TwitterOAuth::setConfig(realpath(__DIR__ . '/ExampleConfig.yaml'));
 
-        // we need to mock the $_SERVER
-        $_SERVER = [
-            'USER'            => 'webiny',
-            'HOME'            => '/home/webiny',
-            'SCRIPT_FILENAME' => '/var/www/projects/webiny/Public/index.php',
-            'SCRIPT_NAME'     => '/index.php',
-            'REQUEST_URI'     => '/batman-is-better-than-superman/?batman=one&superman=two',
-            'DOCUMENT_URI'    => '/index.php',
-            'SERVER_PROTOCOL' => 'HTTP/1.1',
-            'REMOTE_ADDR'     => '192.168.58.1',
-            'SERVER_NAME'     => 'admin.w3.com',
-        ];
+        Request::getInstance()->setCurrentUrl('http://admin.w3.com/batman-is-better-than-superman/?batman=one&superman=two');
+
+        // other tests might change the library, which can cause this test to fail
+        Bridge::setLibrary('\Webiny\Component\TwitterOAuth\Bridge\League\TwitterOAuth');
 
         $instance = TwitterOAuthLoader::getInstance('MyTwitterApp');
         $this->assertInstanceOf('\Webiny\Component\TwitterOAuth\TwitterOAuth', $instance);
